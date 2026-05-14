7e4fd890e863734944d3f951b7d09eefd1a70f9f
skluber <arodriguezzmartinez@gmail.com>
2026-05-14 16:25:43 +0200

    add paint functions

--- javascript.js
+++ javascript.js
@@ -5,7 +5,12 @@ document.body.appendChild(container);
 
 for (let j = 0; j < 16*16; j++) {
     const cell = document.createElement("div");
-    cell.id = "cell";
     cell.className = "cell";
     container.appendChild(cell);
 }
+
+container.addEventListener('mouseover', (event) => {
+    if (event.target.classList.contains("cell")) {
+        event.target.style.backgroundColor = "black";
+    }
+})
